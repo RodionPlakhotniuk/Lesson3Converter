9e4b37bb17d98404403d051360856c3f529187e8
RodionPlakhotniuk <raidertwk@gmail.com>
2025-03-09 15:03:53 +0200

    feat/initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,8 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Degree converter apps\n" +
+                "Version: 1.0\n");
+    }
+}
