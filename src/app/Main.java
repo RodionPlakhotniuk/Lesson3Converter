f97aaece62e19fa592877be0ddd40e22d92870a7
RodionPlakhotniuk <raidertwk@gmail.com>
2025-03-09 15:09:47 +0200

    feat/added functionality for converting fahrenheit to celsius and updated version

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,7 +2,15 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
+        double fahrenheit = 68;
+
         System.out.printf("Degree converter apps\n" +
-                "Version: 1.0\n");
+                "Version: 1.1\n");
+
+        System.out.printf("%.2f fahrenheit = %.2f celsius\n", fahrenheit, convFahrenheitToCelsius(fahrenheit));
+    }
+
+    private static double convFahrenheitToCelsius(double fahrenheit){
+        return (fahrenheit - 32) * 5.0 / 9.0;
     }
 }
